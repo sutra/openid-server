b8e1d7fa18e22ac9b76bea90fd23a81f448db95b
zhoushuqun <zhoushuqun@96094cf4-621f-0410-8965-f16fca8ed9e0>
2007-04-29 17:23:39 +0000

    alpha1 version.

---
+++ tags/openid-server-1.0-alpha1/src/main/java/cn/net/openid/web/ServerServlet.java
@@ -0,0 +1,68 @@
+/**
+ * Created on 2006-10-5 下午05:58:31
+ */
+package cn.net.openid.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import cn.net.openid.OpenidConfiguration;
+
+/**
+ * @author Shutra
+ * 
+ */
+public class ServerServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2796635946888123803L;
+
+	private static final Log log = LogFactory.getLog(ServerServlet.class);
+
+	private SampleServer server;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
+	 */
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		String openidConfigurationBeanName = config.getServletContext()
+				.getInitParameter(OpenidConfiguration.CONFIGURATION_BEAN_NAME);
+		OpenidConfiguration openidConfiguration = (OpenidConfiguration) WebApplicationContextUtils
+				.getWebApplicationContext(config.getServletContext()).getBean(
+						openidConfigurationBeanName);
+
+		server = new SampleServer(openidConfiguration.getOpenidServer());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#service(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			server.processRequest(req, resp);
+		} catch (Exception e) {
+			log.error("error.", e);
+			throw new ServletException(e);
+		}
+	}
+}
---
+++ tags/openid-server-1.0-alpha1/src/main/webapp/member.jsp
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<link rel="openid.server" href="${openIdServer}" />
+	</head>
+	<body>
+		<h3>
+			OpenID Identity Page
+		</h3>
+		<p>
+			This is the identity page for the user
+			<strong>${openIdUsername}</strong>.
+		</p>
+	</body>
+</html>
