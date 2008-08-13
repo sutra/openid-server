ce08399a8dcffa56a7f1191b0524583adff4dfc8
zhoushuqun <zhoushuqun@96094cf4-621f-0410-8965-f16fca8ed9e0>
2008-08-13 12:38:46 +0000

    Tag version jos-1.1.0-alpha.

--- jos-service/src/main/java/cn/net/openid/jos/service/JosService.java
+++ jos-service/src/main/java/cn/net/openid/jos/service/JosService.java
@@ -5,11 +5,15 @@ package cn.net.openid.jos.service;
 
 import java.util.Collection;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.openid4java.server.ServerManager;
+
 import cn.net.openid.jos.domain.Attribute;
 import cn.net.openid.jos.domain.AttributeValue;
+import cn.net.openid.jos.domain.Domain;
 import cn.net.openid.jos.domain.Email;
 import cn.net.openid.jos.domain.EmailConfirmationInfo;
-import cn.net.openid.jos.domain.JosConfiguration;
 import cn.net.openid.jos.domain.Password;
 import cn.net.openid.jos.domain.Persona;
 import cn.net.openid.jos.domain.Site;
@@ -20,15 +24,42 @@ import cn.net.openid.jos.domain.User;
  * 
  */
 public interface JosService {
-	JosConfiguration getJosConfiguration();
+	ServerManager getServerManager(Domain domain);
 
-	String buildOpenidUrl(String username);
+	/**
+	 * Parse domain from the request.
+	 * 
+	 * @param request
+	 *            the HTTP request
+	 * @return the domain if it was found, else null
+	 */
+	Domain parseDomain(HttpServletRequest request);
+
+	/**
+	 * Parse username from the request of the specified domain.
+	 * 
+	 * @param domain
+	 *            the domain
+	 * @param request
+	 *            the HTTP request
+	 * @return the username parsed from the request of the specified domain, if
+	 *         it was not found, null returned
+	 */
+	String parseUsername(Domain domain, HttpServletRequest request);
+
+	Domain getDomain(String id);
+
+	Domain getDomainByName(String name);
+
+	Domain getDomainByName(String name, int type);
+
+	void insertDomain(Domain domain);
 
 	User getUser(String id);
 
-	User getUserByUsername(String username);
+	User getUser(Domain domain, String username);
 
-	User getUser(String username, String passwordPlaintext);
+	User getUser(Domain domain, String username, String passwordPlaintext);
 
 	/**
 	 * Generate a random string for EmailConfiratmionInfo.
